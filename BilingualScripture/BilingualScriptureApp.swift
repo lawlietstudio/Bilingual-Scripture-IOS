bf5105a544bfabf7471b0886e586c7458e0151c9
Mark Ho <mark.ho@vo2-group.com>
2024-07-30 21:38:45 -0400

    clean code

--- BilingualScripture/BilingualScriptureApp.swift
+++ BilingualScripture/BilingualScriptureApp.swift
@@ -2,17 +2,9 @@ import SwiftUI
 
 @main
 struct BilingualScriptureApp: App {
-//    @AppStorage("useDarkMode") private var useDarkMode = false
-    
-    init() {
-        UIScrollView.appearance().bounces = false
-    }
-    
     var body: some Scene {
         WindowGroup {
             CustomTabView()
-//                .preferredColorScheme(useDarkMode ? .dark : .light)
-//                .accentColor(useDarkMode ? .white : .black)
         }
     }
 }
